f864cbabbf2d84ba9c226eb284e05a2df66151e8
Benjamin Cardwell <bencardwell1999@gmail.com>
2025-12-26 13:53:19 -0500

    Add detection of use of built-in Google utilities

--- background.js
+++ background.js
@@ -2,16 +2,16 @@ const googleUrls = [
   "www.google.com"
 ]
 
-const LOG = false
+const LOG = true
 const no_ai_addon = " -ai"
 
 function conditional_log(desc_or_str, str = null) {
   if (LOG) {
     if (str) {
-      conditional_log(desc_or_str, str)
+      console.log(desc_or_str, str)
     }
     else {
-      conditional_log(desc_or_str)
+      console.log(desc_or_str)
     }
   }
 }
@@ -19,8 +19,8 @@ function conditional_log(desc_or_str, str = null) {
 chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
   try {
     let data = await chrome.storage.sync.get("enabled")
-    conditional_log('enabledObj', data)
-    conditional_log('details', details)
+    // conditional_log('enabledObj', data)
+    // conditional_log('details', details)
     if (!data?.enabled) return;
     const urlObj = new URL(details.url);
     // Only listen for google.com/search
@@ -56,7 +56,49 @@ async function getPrevQuery() {
   return data?.prevQuery?.trim() ?? ""
 }
 
+function usingBuiltInUtility(query) {
+  conditional_log("usingBuiltInUtility?")
+  query = query.toLowerCase()
+  // Using basic math
+  if (/[!#+\-$%^*()\[\]]/i.test(query)) {
+    return true
+  }
+  const queryWordSet = new Set(
+    query.split(/\s+/).filter(word => word.length > 0)
+  );
+  const wordUtilities = [
+    "timer", "stopwatch",
+    "weather", "forecast",
+    "define", "synonym", "antonym", "spell",
+    "spinner", 
+    "metronome",
+    "translate",
+    "mod", "choose", "permute",
+  ]
+  for (const wordUtility of wordUtilities) {
+    if (queryWordSet.has(wordUtility)) {
+      return true
+    }
+  }
+  const phraseUtilities = [
+    "near me",
+    "stop watch",
+    "time in", "time is it in",
+    "roll a die", "roll dice", "roll die", "random number",
+    "color picker",
+    "log(", "ln(", "log2(", "sqrt(", "cbrt(", "exp(",
+    "sin(", "cos(", "tan(", "sinh(", "cosh(", "tanh(", "abs(", "sign(",
+  ]
+  for (const phraseUtility of phraseUtilities) {
+    if (query.includes(phraseUtility)) {
+      return true
+    }
+  }
+  return false
+}
+
 async function addNoAiToQuery(urlObj) {
+  conditional_log("addNoAiToQuery")
   let currentQuery = urlObj.searchParams.get("q")
   conditional_log('orig_query', currentQuery)
   if (!currentQuery) {
@@ -79,7 +121,7 @@ async function addNoAiToQuery(urlObj) {
     }
   }
   // Don't change the URL if query already has " -ai"  (prevent infinite loop)
-  else if (currentQuery.includes(no_ai_addon)) {  
+  else if (currentQuery.includes(no_ai_addon)) {
     conditional_log('already has (or falsy)')
     return null
   }
@@ -88,9 +130,16 @@ async function addNoAiToQuery(urlObj) {
     prevQuery = await getPrevQuery()
     // Don't modify their search if we detect they manually deleted the " -ai"
     if (currentQuery !== prevQuery
-      && currentQuery === removeNoAiFromQuery(prevQuery)) {      
+      && currentQuery === removeNoAiFromQuery(prevQuery)) {
       return null
     }
+
+    // If the user is trying to use a Google built-in utility
+    if (usingBuiltInUtility(currentQuery)) {
+      conditional_log("yes")
+      return null
+    }
+
     // Add " -ai" to their query
     newQuery = `${currentQuery}${no_ai_addon}`
   }
